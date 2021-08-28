c8c52317826da6290acca02ce89231061d20753d
daidr <daidr@daidr.me>
2021-08-28 23:44:01 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -72,7 +72,7 @@ self.addEventListener('fetch', event => {
             } else {
                 const svg = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg width="100%" height="100%" viewBox="0 0 ${width} ${height}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+<svg width="${width}px" height="${height}px" viewBox="0 0 ${width} ${height}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
     <rect x="0" y="0" width="${width}" height="${height}" style="fill:#${bgcolor};"/>
     <text x="50%" y="50%" style="font-family: ${FontFamily};text-anchor:middle;alignment-baseline:middle;font-size:${width > height ? height / 4 : width / 7}px;fill:#${fgcolor};">${text}</text>
 </svg>
